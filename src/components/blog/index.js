09c8d05a5d04fdf754209a6ea0286000572f52f0
Mitia Jessica <mitiah.jessica@gmail.com>
2026-05-04 14:35:48 +0300

    install dependencies

---
+++ src/components/blog/index.js
@@ -0,0 +1,7 @@
+export { default as BlogHero } from './BlogHero';
+export { default as FeaturedArticle } from './FeaturedArticle';
+export { default as CategoryFilter } from './CategoryFilter';
+export { default as ArticlesGrid } from './ArticlesGrid';
+export { default as Pagination } from './Pagination';
+export { default as ArticleCard } from './ArticleCard';
+export { default as CategoryBadge } from './CategoryBadge';
